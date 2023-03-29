bf265e6e38fb8a6b402c8bdbf4cc365b96901bc5
Nikky1092 <mariannaniko1092@gmail.com>
2023-03-29 21:33:11 +0000

    JS Random Card Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,50 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let klass = ["heart", "spade", "club", "diamond"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suit = {
+    heart: "♥",
+    spade: "♠",
+    club: "♣",
+    diamond: "♦"
+  };
+
+  function getArrayString(x) {
+    let n = Math.floor(Math.random() * x.length);
+    let string = x[n];
+    return string;
+  }
+
+  let randomString = getArrayString(klass);
+  let content = document.getElementsByClassName("card");
+  for (let i = 0; i < content.length; i++) {
+    let card = content[i];
+    card.className = card.className + " " + randomString;
+    let icon = suit[randomString];
+    let element = card.getElementsByTagName("span");
+
+    for (let i = 0; i < element.length; i++) {
+      let tag = element[i];
+      tag.innerHTML = icon;
+    }
+  }
+
+  let randomNumber = getArrayString(number);
+  let p = document.getElementById("number");
+  p.innerHTML = randomNumber;
 };
